d1d73842ea8d2b36fac9c90dad5ec73407ee7dea
dev <dev@dev.com>
2022-11-30 19:53:45 +0800

    add icon

--- forge.config.js
+++ forge.config.js
@@ -2,27 +2,52 @@ const os = require('os')
 const platform = os.platform()
 const config = {
   packagerConfig: {
-    extraResources:['./lib/**', './flowscript/**','./userData/**','./doc/**','./logs/**','./ref_**/**']
+    name: 'Web3RPA',
+    icon: './src/images/icon/icon',
+    extraResources:['./src/**']
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
+      // all
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin']
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      // Win
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        icon: './src/images/icon/icon.icon'
+      }
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      // Mac
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: './src/images/icon/icon.icns'
+      }
+    }
   ],
-};
+  publishers:[
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'rpa',
+          name: 'wecaterpillar/web3rpa-electron'
+        },
+        prerelease: true
+      }
+    }
+  ],
+  // plugins: [
+  //   {
+  //     name: '@electron-forge/plugin-webpack',
+  //     config: {
+  //       mainConfig: './webpack.main.config.js'
+  //     }
+  //   }
+  // ]
+}
 
 module.exports = config
