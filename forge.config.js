097b454d94f45e8faf3210732e0bd3874d8b3b1a
dev <dev@dev.com>
2022-12-06 01:23:42 +0800

    change forge config

--- forge.config.js
+++ forge.config.js
@@ -10,9 +10,9 @@ const config = {
     ignore: [
       '.env','.idea','.vscode','yarn.lock','.yarnclean','.gitignore','web3rpa-electron.iml'
       ,'webpack.*.config.js','webpack.rules.js','forge.config.js','README.md'
-      ,'w3rpa','ref_rpa','doc','fix_make','dist/native_modules'
+      ,'/w3rpa','/ref_rpa','doc','/fix_make','dist/native_modules','/release'
     ],
-    asar: true,
+    asar: false,
     // asarUnpack: [
     //   './src/rpa/browser.js',
     //   './src/rpa/dataUtil.js'
